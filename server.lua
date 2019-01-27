bf8c4077624ab006897e51b2a7e4351d6b06512b
Sun <30409932+Origintk@users.noreply.github.com>
2019-01-27 10:01:35 -0200

    up

---
+++ server.lua
@@ -0,0 +1,33 @@
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+
+vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP")
+vRPnitro = {}
+Tunnel.bindInterface("nitro",vRPnitro)
+	
+function vRPnitro.chamada()
+local user_id = vRP.getUserId(source)
+local player = vRP.getUserSource(user_id)
+if vRP.tryGetInventoryItem(user_id,"nitro",1,false) then
+ TriggerClientEvent("pNotify:SendNotification", player, {
+			text = "Nitro Ativado",
+			type = "success",
+			progressBar = true,
+			queue = "lmao",
+			timeout = 2000,
+			layout = "CenterLeft"
+			})
+	return true
+	end
+	TriggerClientEvent("pNotify:SendNotification", player, {
+			text = "Você Não tem Nitro",
+			type = "error",
+			progressBar = true,
+			queue = "lmao",
+			timeout = 2000,
+			layout = "CenterLeft"
+			})
+	return false
+
+end
